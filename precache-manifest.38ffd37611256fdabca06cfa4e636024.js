037d99c93a07ad736ea819288c5a9ad9219d9b55
JanClod <fia.yan@mail.ru>
2022-04-29 17:26:35 +0300

    Updates

---
+++ precache-manifest.38ffd37611256fdabca06cfa4e636024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6c35561a40eb790d786ba2b9998261",
+    "url": "/git-hub-pages/index.html"
+  },
+  {
+    "revision": "a700a64c335565838610",
+    "url": "/git-hub-pages/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "11930786429232958006",
+    "url": "/git-hub-pages/static/js/2.83ec6f1e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/git-hub-pages/static/js/2.83ec6f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a700a64c335565838610",
+    "url": "/git-hub-pages/static/js/main.118ecfd8.chunk.js"
+  },
+  {
+    "revision": "f4d93ef6c333eb3c1ee0",
+    "url": "/git-hub-pages/static/js/runtime-main.cadb02a7.js"
+  }
+]);
